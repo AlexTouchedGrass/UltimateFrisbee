8b307d95c497d5d478c1028321b638636d0909e4
AlexTouchedGrass <dean-potts.622237@swl.k12.oh.us>
2024-02-28 10:56:59 -0500

    If our last few projects were like this I would rolling in 100's.

---
+++ src/UltimateCaptain.java
@@ -0,0 +1,3 @@
+public class UltimateCaptain extends UltimatePlayer {
+
+}
---
+++ src/UltimateTeam.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+
+public class UltimateTeam {
+    public static void main(String[] args) {
+
+        ArrayList<Coach> coaches = new ArrayList<Coach>();
+        ArrayList<UltimatePlayer> ultimatePlayers = new ArrayList<UltimatePlayer>();
+
+        //Create Coach Objects.
+
+
+        //Add Coach Objects to list.
+
+
+        //Print Coach List.
+        for (Coach c: coaches) {
+            System.out.println(coaches);
+            System.out.println();
+        }
+
+        //Create Player Objects.
+
+
+        //Add Player Objects to list.
+
+
+        //Print Player List.
+        for (UltimatePlayer p: ultimatePlayers) {
+            System.out.println(ultimatePlayers);
+            System.out.println();
+        }
+
+        //Down Here Is Where Post-Creation Changes Are Made.
+
+
+    }
+}
