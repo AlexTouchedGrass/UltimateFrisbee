ffe91749c7614619c9316e8a09be20c4e6a7110d
AlexTouchedGrass <144457672+AlexTouchedGrass@users.noreply.github.com>
2024-03-01 11:40:10 -0500

    Update UltimateCaptain.java

--- src/UltimateCaptain.java
+++ src/UltimateCaptain.java
@@ -1,3 +1,20 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class UltimateCaptain extends UltimatePlayer {
 
+    private boolean type;
+
+    
+
+    //Constructor
+    public UltimateCaptain(String firstName, String lastName, int jerseyNumber, String position) {
+        //Wait until we make mutators - make interior last.
+        super(firstName, lastName, jerseyNumber, position);
+    }
+
+    //Accessors
+    public String toString(){
+        return super.toString();
+    }
 }
